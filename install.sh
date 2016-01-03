c91a8b03ef02efa105b4a97f5a069c7a742d0d51
Phil Thompson <phil@electricvisions.com>
2016-01-03 17:35:04 +0000

    fix for efibootmgr defaulting to sda (typo, aaaah)

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #### VERSION ####
-echo 'Arch Install Script Version 0.4.18'
+echo 'Arch Install Script Version 0.4.19'
 echo '=================================='
 echo ''
 
@@ -195,7 +195,7 @@ if [[ $UEFI = true ]]; then
   SYSLINUX_CONFIG='/boot/EFI/syslinux/syslinux.cfg'
   BOOTLOADER_EXTRA="mkdir -p /boot/EFI/syslinux
 cp -r /usr/lib/syslinux/efi64/* /boot/EFI/syslinux
-efibootmgr -c -d /dev${DRIVE} -l /EFI/syslinux/syslinux.efi -L Syslinux
+efibootmgr -c -d /dev/${DRIVE} -l /EFI/syslinux/syslinux.efi -L Syslinux
 "
 else
   PARENT=''
